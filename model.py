17e8180ff5481b23fe763cb42ff873dc42b3d5cd
Alejandro <alejandronotario@gmail.com>
2024-05-07 22:01:00 +0200

    modelo adaptado

--- model.py
+++ model.py
@@ -13,8 +13,15 @@ def compute(texto):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-        {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
-        {"role": "user", "content": "Sentiment analysis of the following text: {}".format(texto)}
+        {"role":"system","content":
+          "This is an application to help call center employees"
+          "Assistant takes inputs from users and determines its positive neutral and negative weights."
+          "This weights are in 0-1 scale and their sum is 1."
+          "Assistant must print the weights"
+          "Assistant must print the input text"
+          "Besides, if the negative weight is above 0.5, the Assistant must suggest a text in Spanish."
+          "If the input text is a query te Assistant must suggest a solution in Spanish"},
+        {"role": "user", "content": "{}".format(texto)}
       ]
     )
     prediction = response.choices[0].message.content
